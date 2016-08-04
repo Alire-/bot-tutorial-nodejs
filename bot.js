5d041eb4714f665e7c36dd1bb12883ff541fb8eb
Alire- <sylaswynn@gmail.com>
2016-08-04 09:50:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "A bag, please.";
 
   options = {
     hostname: 'api.groupme.com',
