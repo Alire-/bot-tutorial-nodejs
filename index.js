7f48a3a42da7d0d3abcf7eedae778eb7b18f8a89
Alire- <sylaswynn@gmail.com>
2016-08-04 11:36:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ http        = require('http');
 director    = require('director');
 cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
-dickcontrol = require('./dickcontrol.js')
 
-router1 = new director.http.Router({
+router = new director.http.Router({
   '/' : {
     post: bot.respond,
     get: ping
   }
 });
-
 
 server = http.createServer(function (req, res) {
   req.chunks = [];
@@ -26,12 +24,10 @@ server = http.createServer(function (req, res) {
   });
 });
 
-
-
 port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
   this.res.writeHead(200);
-  this.res.end("Eat a bag of dicks.");
+  this.res.end("Hey, I'm Cool Guy.");
 }
