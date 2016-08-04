cf840db8817d79a7a974a35bdc2e3c1f3dfdf57f
Alire- <sylaswynn@gmail.com>
2016-08-04 12:27:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ http        = require('http');
 director    = require('director');
 cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
-okc = require('./okc.js');
+bot         = require('./okc.js');
 
 router = new director.http.Router({
   '/' : {
